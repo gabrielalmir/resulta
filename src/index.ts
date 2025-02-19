a85a8f4043f38fcfb4646f0091f1010c3683c499
gabrielalmir <gabr.almir@gmail.com>
2025-02-18 22:53:56 -0300

    feat: add jsdocs to match

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,15 @@ export function err<E>(error: E): Err<E> {
 }
 
 
+/**
+ * Executes a provided asynchronous function and returns a `Result` type.
+ *
+ * @template T - The type of the successful result.
+ * @template E - The type of the error result.
+ * @param {() => Promise<T>} fn - The asynchronous function to execute.
+ * @returns {Promise<Result<T, E>>} A promise that resolves to a `Result` type,
+ *                                  containing either the successful result or an error.
+ */
 export async function match<T, E>(fn: () => Promise<T>): Promise<Result<T, E>> {
     try {
         const result = await fn();
